526c341fa1d6f9471d65995f92697f1c246a1fac
Alexandr Druzhinin <drug2004@bk.ru>
2013-10-11 10:28:54 +0700

    Issue #17 implemented

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-bin/geoviewer bin/osm-no-label.xml
+bin/geoviewer bin/osm-no-label.json
--- src/settings.d
+++ src/settings.d
@@ -1,26 +1,23 @@
 module settings;
 
 import std.file: readText;
-import arsd.dom: Document, Element;
 import std.conv: to;
-import std.typecons: Tuple;
+import std.json: JSONValue, parseJSON;
 
 class Settings {
+private:	
+	string filename_;
+	JSONValue json_;
 	
-	private string filename_;
-	private Document doc_;
-	private Element sets_; 
-	
-	public this(string filename) {
+public:
+
+	this(string filename) {
 		filename_ = filename;
 	
-		doc_ = new Document(readText(filename), true, true);
-
-		sets_ = doc_.requireSelector("settings");
+		json_ = parseJSON(readText(filename_));
 	}
 	
-	@property 
-	public auto get(T, alias settings_name)() {
-        return (sets_.getElementsByTagName(settings_name)[0].innerText()).to!T;
+	@property get(T, alias string settings_name)() {
+		return (json_[settings_name].str).to!T;
     }
 }

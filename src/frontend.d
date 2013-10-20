1672de1616f34f4853179b54d0ea05b9fe389ca7
Alexandr Druzhinin <drug2004@bk.ru>
2013-10-20 22:35:16 +0700

    Code refactored. SDLApp added.

--- src/frontend.d
+++ src/frontend.d
@@ -1,6 +1,6 @@
 module frontend;
 
-import std.conv: text, to;
+import std.conv: text;
 import std.concurrency: thisTid, Tid, send, prioritySend, receiveTimeout, OwnerTerminated, Variant;
 import std.datetime: dur, StopWatch;
 import std.stdio: stderr, writeln, writefln;
@@ -8,27 +8,21 @@ import std.exception: enforce;
 import std.traits: isUnsigned;
 import core.thread: Thread;
 
-import derelict.sdl2.sdl;
-import derelict.opengl3.gl3;
-
+import sdlapp: SDLApp;
 import renderer: Renderer;
 import tile: Tile, world2tile, tile2geodetic, geodetic2tile, tile2world;
 import backend: BackEnd;
 
 // Frontend receives user input from OS and sends it to backend,
 // receves from backend processed data and renders it
-class FrontEnd
+class FrontEnd : SDLApp
 {
 private:
 	Renderer renderer_;
-	SDL_Window* sdl_window_;
-	SDL_GLContext gl_context_;
 
     BackEnd backend_;
     Tid backend_tid_;
     string url_, cache_path_;
-
-    uint mouse_x_, mouse_y_;
 
     /// current requested tile batch id
     size_t batch_id_;
@@ -56,36 +50,7 @@ public:
 
     this(uint width, uint height, double lon, double lat, string url, string cache_path)
 	{
-		DerelictSDL2.load();
-	    DerelictGL3.load();
-
-	    if (SDL_Init(SDL_INIT_VIDEO) < 0)
-	        throw new Exception("Failed to initialize SDL: " ~ SDL_GetError().text);
-
-	    scope(failure) SDL_Quit();
-
-	    // Set OpenGL version
-	    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-	    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
-
-	    // Set OpenGL attributes
-	    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-	    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-
-	    sdl_window_ = SDL_CreateWindow("Geoviewer",
-	        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-	        width, height, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
-
-	    if (!sdl_window_)
-	    	throw new Exception("Failed to create a SDL window: " ~ SDL_GetError().text);
-	    scope(failure) SDL_DestroyWindow(sdl_window_);
-
-	    gl_context_ = SDL_GL_CreateContext(sdl_window_);
-	    if (gl_context_ is null)
-	    	throw new Exception("Failed to create a OpenGL context: " ~ SDL_GetError().text);
-	    scope(failure) SDL_GL_DeleteContext(gl_context_);
-
-	    DerelictGL3.reload();
+		super("Geoviewer", width, height);
 
 		renderer_ = new Renderer(width, height);
         renderer_.camera.eyes = geodetic2tile(lon, lat, 0).tile2world;
@@ -94,22 +59,62 @@ public:
         cache_path_ = cache_path;
 
         launchBackend();
+
+        mouseMotionHandler = (int x, int y, bool rbtn_pressed)
+        {
+            //auto t = renderer_.camera.mouse2world(x, y, 0).world2tile;
+            //writefln("tile: %s", t);
+            //writefln("geodetic: %s", t.tile2geodetic);
+
+            if (rbtn_pressed) {
+                renderer_.camera.scrollingEnabled = true;
+            } else {
+                renderer_.camera.scrollingEnabled = false;
+            }
+
+            if(renderer_.camera.doScrolling(x, y))
+            {
+                requestNewTileBatch();
+            }
+        };
+
+        mouseWheelHandler = (int x, int y)
+        {
+            if(y)
+            {
+                if (y > 0)
+                {
+                    renderer_.camera.multiplyScale(1.025);
+                    requestNewTileBatch();
+                }
+                else
+                {
+                    renderer_.camera.multiplyScale(.975);
+                    requestNewTileBatch();
+                }
+            }
+        };
+
+        drawHandler = ()
+        {
+            // process messages from BackEnd
+            receiveMsg();
+            // draw result of processing events and
+            // receiving messages
+            renderer_.draw(mouse_x_, mouse_y_);
+        };
+
 	}
 
-	void close()
-	{
-		renderer_.close();
-		/* Delete our opengl context, destroy our window, and shutdown SDL */
-	    SDL_GL_DeleteContext(gl_context_);
-	    SDL_DestroyWindow(sdl_window_);
-	    SDL_Quit();
-	}
+    override void close()
+    {
+        renderer_.close();
+        super.close();
+    }
 
 	// run frontend in the current thread
-	void run()
+	override void run()
 	{
-        enum FRAMES_PER_SECOND = 60;
-
         enforce(backend_tid_ != Tid.init);
 
         // without delay messages can be missed by backend TODO: the reason isn't known
@@ -117,34 +122,7 @@ public:
         // init the first tile batch loading
         requestNewTileBatch();
 
-        //The frame rate regulator
-        StopWatch fps;
-        fps.start();
-        while(true)
-		{
-
-            // process events from OS, if result is
-			// false then break loop
-			if(!processEvents())
-				break;
-			// process messages from BackEnd
-			receiveMsg();
-			// draw result of processing events and
-			// receiving messages
-			renderer_.draw(mouse_x_, mouse_y_);
-
-        	SDL_GL_SwapWindow(sdl_window_);
-
-            auto t = fps.peek.msecs;
-            if(t < 1000 / FRAMES_PER_SECOND)
-            {
-                //Sleep the remaining frame time
-                auto delay = (1000 / FRAMES_PER_SECOND) - t;
-                Thread.sleep(dur!"msecs"(delay));
-            }
-            //reset the frame timer
-            fps.reset();
-		}
+        super.run();
 	}
 
     /// using camera defines tiles that are in camera frustum and therefore should be loaded
@@ -166,82 +144,6 @@ public:
             // request tile image from backend
             backend_tid_.send(batch_id_, tile.x, tile.y, tile.zoom);
         }
-    }
-
-	bool processEvents()
-	{
-    	bool is_running = true;
-        // handle all SDL events that we might've received in this loop iteration
-        SDL_Event event;
-        while (SDL_PollEvent(&event)) {
-            switch(event.type){
-                // user has clicked on the window's close button
-                case SDL_QUIT:
-	                is_running = false;
-                    break;
-                case SDL_KEYUP:
-                	switch (event.key.keysym.sym) {
-                		// if user presses ESCAPE key - stop running
-                		case SDLK_ESCAPE:
-                			is_running = false;
-                			break;
-                		default:{}
-                	}
-                	break;
-                case SDL_KEYDOWN:
-                	switch (event.key.keysym.sym) {
-                		case SDLK_UP:
-                			break;
-                		case SDLK_DOWN:
-                			break;
-                		case SDLK_RIGHT:
-                			break;
-                		case SDLK_LEFT:
-                			break;
-                		default:{}
-                	}
-                	break;
-	            case SDL_MOUSEMOTION:
-                    mouse_x_ = event.motion.x;
-                    mouse_y_ = event.motion.y;
-
-                    //auto t = renderer_.camera.mouse2world(mouse_x_, mouse_y_, 0).world2tile;
-                    //writefln("tile: %s", t);
-                    //writefln("geodetic: %s", t.tile2geodetic);
-
-                    if (event.motion.state & SDL_BUTTON_RMASK) {
-                        renderer_.camera.scrollingEnabled = true;
-                    } else {
-                        renderer_.camera.scrollingEnabled = false;
-                    }
-
-                    if(renderer_.camera.doScrolling(mouse_x_, mouse_y_))
-                    {
-                        requestNewTileBatch();
-                    }
-                break;
-	            case SDL_MOUSEBUTTONDOWN:
-	                break;
-                case SDL_MOUSEWHEEL:
-                    if(event.wheel.y)
-                    {
-                        if (event.wheel.y > 0)
-                        {
-                            renderer_.camera.multiplyScale(1.025);
-                            requestNewTileBatch();
-                        }
-                        else
-                        {
-                            renderer_.camera.multiplyScale(.975);
-                            requestNewTileBatch();
-                        }
-                    }
-                    break;
-                default:
-                    break;
-            }
-        }
-        return is_running;
     }
 
     void receiveMsg()
